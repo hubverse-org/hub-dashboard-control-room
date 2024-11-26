2a93df4d9426e6de298ac20db7b272e86b7edc43
Zhian N. Kamvar (UMass) <zkamvar@umass.edu>
2024-11-26 09:13:27 -0800

    what

--- appHelper.py
+++ appHelper.py
@@ -77,6 +77,7 @@ def list_repositories():
     '''
     newbies = os.environ.get("NEW_REPOS")
     print(newbies)
+    print(newbies == "")
     print(newbies not in set(["", "null", "false", " "]))
     if newbies is not None and newbies not in set(["", "null", "false", " "]):
         print("loading")
